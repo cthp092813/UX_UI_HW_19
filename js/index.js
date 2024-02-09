cba9618fab2996daf1319315f565056b04076de2
cthp092813 <144400135+cthp092813@users.noreply.github.com>
2024-02-09 00:09:56 -0700

    CSS Iterations

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 $(window).on("mousewheel", function() {
-    var the_position = $(document).scrollTop();
-    if (the_position > 400) 
-      {
-        $(".content").fadeTo("fast", 0, "swing");
-      }
-      else
-      {
-        $(".content").fadeTo("fast", 1, "swing");
-      }
+  var the_position = $(document).scrollTop();
+  if (the_position > 400) 
+    {
+      $(".content").fadeTo("fast", 0, "linear");
+    }
+    else
+    {
+      $(".content").fadeTo("fast", 1, "linear");
+    }
 });
